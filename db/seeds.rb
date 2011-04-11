50cbef6c0acdaf50733917cba27413e4b3b95a48
Gelir <zjovanov@hsz-t.ch>
2011-04-11 14:40:20 +0200

    Prettier Listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+
+Product.create(:title => 'Programming Ruby 1.9',
+  :description =>
+	%{<p>
+	  Ruby is the fastest growing and most exciting dynamic language out
+	  there. If you need to get working programs delivered fast, you should
+ 	  add Ruby to your toolbox.
+	</p>},
+:image_url => '/images/ruby.jpg',
+:price => 49.50)
