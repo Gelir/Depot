4bf7c5a02a094b258ef8f162aaf77b50d2e30a97
Gelir <zjovanov@hsz-t.ch>
2011-06-05 19:24:59 +0200

    Chapter 12 G1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,10 +48,13 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to(store_url) }
 	format.js   { @current_item = @line_item }
-        format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
+        format.xml  { render :xml => @line_item, 
+	   :status => :created, :location => @line_item }
+
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @line_item.errors, 
+	   :status => :unprocessable_entity }
       end
     end
   end
--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -27,6 +27,7 @@ class LineItemsControllerTest < ActionController::TestCase
   assert_difference('LineItem.count') do
   xhr :post, :create, :product_id => products(:ruby).id
   end
+
   assert_response :success
   assert_select_rjs :replace_html, 'cart' do
   assert_select 'tr#current_item td', /Programming Ruby 1.9/
